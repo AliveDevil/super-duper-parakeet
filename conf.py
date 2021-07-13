4afcb1d0fbbf4137d02077f214ea2b93673edbf7
AliveDevil <alivedevil@users.noreply.github.com>
2021-07-13 10:29:35 +0200

    Fix

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ author = 'sphinx-action Test Suite'
 extensions = [
 	"sphinx_rtd_theme",
 	"sphinx_tabs.tabs",
-	"myst-parser"
+	"myst_parser"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
