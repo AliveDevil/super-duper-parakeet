f53782a72b9fa817eb1ac2fa69603cd8f7b942e5
AliveDevil <alivedevil@users.noreply.github.com>
2021-07-13 09:57:34 +0200

    Update

--- conf.py
+++ conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme;
 
 # -- Project information -----------------------------------------------------
 
@@ -28,7 +29,7 @@ author = 'sphinx-action Test Suite'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	sphinx_rtd_theme
+	"sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
